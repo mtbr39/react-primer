e44900c4ca2a6462fa1f75710383c77d82854f04
mtbr39 <nm1552jp@gmail.com>
2024-06-06 04:17:16 +0900

    header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,44 @@
-// src/components/Layout.js
-import React from 'react';
-import { Box } from '@primer/react';
+import React from 'react'
+import { Box } from '@primer/react'
+import { Header } from '@primer/react'
+import { MarkGithubIcon } from '@primer/octicons-react'
+import { Octicon } from '@primer/react'
 
 const Layout = ({ children }) => {
   return (
-    <Box>
-      <header>Header</header>
+    <Box textAlign="center">
+      <Header
+        sx={{
+          backgroundColor: 'muted'
+        }}>
+        <Header.Item>
+          <Header.Link
+            href="#"
+            sx={{
+              fontSize: 2,
+            }}
+          >
+            <Octicon
+              icon={MarkGithubIcon}
+              size={32}
+              sx={{
+                marginRight: 2,
+              }}
+            />
+            <span>GitHub</span>
+          </Header.Link>
+        </Header.Item>
+        <Header.Item full>Menu</Header.Item>
+        <Header.Item
+          sx={{
+            mr: 0,
+          }}
+        ></Header.Item>
+      </Header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      {/* <footer>Footer</footer> */}
     </Box>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
